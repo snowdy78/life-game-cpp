873e870ff368e63197b9c775f90ff50343bbdbfe
themrleoner12@mail.ru <themrleoner12@mail.ru>
2023-04-04 16:16:52 +0700

    update 0.8.1

--- Life Game.cpp
+++ Life Game.cpp
@@ -7,5 +7,12 @@
 int main()
 {
 	sf::RenderWindow window({ rn::Settings::getResolution().x, rn::Settings::getResolution().y }, "Life Game", rn::Settings::getWindowMode());
+	sf::Image icon;
+	if (!icon.loadFromFile("icon.png"))
+	{
+		std::cout << "[ERROR] Can't load Icon\n";
+		throw;
+	}
+	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 	rn::MenuBranch::start_branch(new lg::MainMenu(window));
 }
